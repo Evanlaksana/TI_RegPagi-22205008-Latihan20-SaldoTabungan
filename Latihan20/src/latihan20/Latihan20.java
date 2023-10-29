bd8d8905b50461793e21faea9cb7ba9cdcf81ba6
Evanlaksana <132762464+Evanlaksana@users.noreply.github.com>
2023-10-29 22:10:14 +0700

    Update Latihan20.java

--- Latihan20/src/latihan20/Latihan20.java
+++ Latihan20/src/latihan20/Latihan20.java
@@ -2,7 +2,7 @@
 Nama : Evan Eka Laksana
  * Kelas : PBO1
  * NIM : 222050008
- * Deskripsi Program : Program ini berisi program untuk menampilkan Program Target Saldo Tabungan
+ * Deskripsi Program : Program ini berisi program untuk menampilkan Target Saldo Tabungan
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
